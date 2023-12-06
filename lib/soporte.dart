52fd1910818d6ac4307c69e69e1322cf15079612
jean982 <112441602+jean982@users.noreply.github.com>
2023-12-05 20:41:47 -0500

    queda

--- lib/soporte.dart
+++ lib/soporte.dart
@@ -7,6 +7,7 @@ class Soport extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: ContactScreen(),
     );
   }
