90a2cdbe79184a020ad2fd872edcc27895be986d
GiusseppeALor <112878090+GiusseppeALor@users.noreply.github.com>
2023-12-06 18:32:12 -0500

    Avance DB V1

---
+++ lib/db/constant.dart
@@ -0,0 +1,6 @@
+/*user = "72575642"
+password = "7jl5wo3pGZ1G53Vf" */
+
+const MONGO_CONN_URL =
+    "mongodb+srv://72575642:7jl5wo3pGZ1G53Vf@flutterispm.4ownr1m.mongodb.net/?retryWrites=true&w=majority";
+const USER_COLLECTION = "Users";
