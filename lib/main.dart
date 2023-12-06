3d2191839354b2ee584254443d70a0004008d1fb
GiusseppeALor <112878090+GiusseppeALor@users.noreply.github.com>
2023-12-06 18:49:36 -0500

    Revert "Revert "a""
    This reverts commit 282e76b36055b3f33589840657af7c3510294e0f.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/2.dart';
 import 'package:flutter_application_2/afilio.dart';
+import 'package:flutter_application_2/db/mongodb.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await MongoDatabase.connect();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
