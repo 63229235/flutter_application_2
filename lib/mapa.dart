4d066b164e9d57a557666f8228fbc502652ada5d
Piero Revolledo <112639712+63229235@users.noreply.github.com>
2023-12-07 00:41:06 -0500

    update-7-12-23

--- lib/mapa.dart
+++ lib/mapa.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/home_controller.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
-class Mapa extends StatefulWidget {
-  const Mapa({super.key});
-
-  @override
-  State<Mapa> createState() => _MapaState();
-}
-
-class _MapaState extends State<Mapa> {
-  final _initialCameraPosition = CameraPosition(
-    target: LatLng(-12.0467946, -77.0147041),
-    zoom: 17,
-  );
+class Mapa extends StatelessWidget {
+  const Mapa({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _initialCameraPosition,
-      ),
+    return ChangeNotifierProvider<HomeController>(
+      create: (_) {
+        final controller = HomeController();
+        controller.onMarkerTap.listen((String id) {
+          print("got to $id");
+        });
+        return controller;
+      },
+      child: Scaffold(
+          body: Consumer<HomeController>(
+        builder: (_, controller, __) => GoogleMap(
+          markers: controller.markers,
+          onMapCreated: controller.onMapCreated,
+          initialCameraPosition: controller.initialCameraPosition,
+          onTap: controller.onTap,
+        ),
+      )),
     );
   }
 }
