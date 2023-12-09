285cbea7be37bcee20436729db20a5bf0ebd1942
Piero Revolledo <112639712+63229235@users.noreply.github.com>
2023-12-09 11:10:37 -0500

    update

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart' show ChangeNotifier;
+import 'package:flutter/material.dart';
 import 'package:flutter_application_2/utils/map_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -6,6 +9,9 @@ class HomeController extends ChangeNotifier {
   final Map<MarkerId, Marker> _markers = {};
 
   Set<Marker> get markers => _markers.values.toSet();
+
+  final _markersController = StreamController<String>.broadcast();
+  Stream<String> get onMarkerTap => _markersController.stream;
 
   final initialCameraPosition = CameraPosition(
     target: LatLng(-12.0467946, -77.0147041),
@@ -17,12 +23,28 @@ class HomeController extends ChangeNotifier {
   }
 
   void onTap(LatLng position) {
-    final markerId = MarkerId(_markers.length.toString());
+    final id = markers.length.toString();
+    final markerId = MarkerId(id);
     final marker = Marker(
       markerId: markerId,
       position: position,
+      draggable: true,
+      anchor: const Offset(0.5, -1),
+      icon: BitmapDescriptor.defaultMarkerWithHue(55),
+      onTap: () {
+        _markersController.sink.add(id);
+      },
+      onDragEnd: (newPosition) {
+        print("new position $newPosition");
+      },
     );
     _markers[markerId] = marker;
     notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _markersController.close();
+    super.dispose();
   }
 }
