8470197cfdb7b063f201d11fbb255752f35e5d68
Piero Revolledo <112639712+63229235@users.noreply.github.com>
2023-12-09 11:37:54 -0500

    update-9-12

---
+++ lib/request_permission/request_permission_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/request_permission/request_permissions_controller.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class RequestPermissionPage extends StatefulWidget {
+  const RequestPermissionPage({super.key});
+
+  @override
+  State<RequestPermissionPage> createState() => _RequestPermissionPageState();
+}
+
+class _RequestPermissionPageState extends State<RequestPermissionPage> {
+  final _controller = RequestPermissionController(Permission.locationWhenInUse);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      ),
+    );
+  }
+}
